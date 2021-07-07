b3a5cbaa41452dfad12bddd1c3e767ce026ec5e4
Hasna M.A.F <it18200034@my.sliit.lk>
2021-07-07 18:19:20 +0530

    Catalog project

---
+++ Catalog/Controllers/ItemsController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Catalog.Repositories;
+using System.Collections.Generic;
+using Catalog.Entities;
+
+namespace Catalog.Controllers 
+{
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly InMemItemsRepository repository;
+
+        public ItemsController()
+        {
+            repository  = new InMemItemsRepository();
+        }
+        // GET / items
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            var items = repository.GetItems();
+            return items;
+        }
+    }
+    
+}
